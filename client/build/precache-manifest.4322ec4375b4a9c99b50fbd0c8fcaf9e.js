0716b1a92bdac418f6204763a6da54b4f36e12b3
Julian Bull <julianbullmagic@gmail.com>
2021-01-19 12:32:58 +1100

    ffff

---
+++ client/build/precache-manifest.4322ec4375b4a9c99b50fbd0c8fcaf9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6ce8642fc1316155fab8c5f88d0fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4610f85e44cba3a7252",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "c34dbe9c25b08cf870e0",
+    "url": "/static/css/main.7276474f.chunk.css"
+  },
+  {
+    "revision": "b4610f85e44cba3a7252",
+    "url": "/static/js/2.b55907ef.chunk.js"
+  },
+  {
+    "revision": "c34dbe9c25b08cf870e0",
+    "url": "/static/js/main.f8d1553d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
