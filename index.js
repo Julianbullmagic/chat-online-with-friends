3410e41b022f9615b6e804da1cd8c15bfcaeb8bb
Julian Bull <julianbullmagic@gmail.com>
2021-01-19 11:45:23 +1100

    fff

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const io = require("socket.io")(server);
 const config = require("./config/key");
 
 
+
+
 const mongoose = require("mongoose");
 const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
